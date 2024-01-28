a8add49625d69147bbdd04f9492a7679f47d1a14
Edward Lim <ed.lim199@gmail.com>
2024-01-29 09:54:23 +1100

    successful status page but without popup

--- src/pages/Status.jsx
+++ src/pages/Status.jsx
@@ -13,17 +13,21 @@ const Status = ({ needPopup }) => {
       const response = await fetch(`http://127.0.0.1:${portForBackend}`);
       const data = await response.json();
 
-      for (const s in data.joiningInfo) {
-        if (s.id === id) {
-          console.log('line 18');
-          setStatus(s);
+      for (const r in data.requests) {
+        const requestItem = data.requests[r];
+        if (requestItem.id == id) {
+          console.log("found request item");
+          console.log(requestItem);
+          setStatus(requestItem);
           break;
         }
       }
-      for (const r in data.requests) {
-        if (r.id === id) {
-          console.log('line 25');
-          setStatus(prev => {prev.title = r.title; console.log(prev); return prev});
+      for (const s in data.joiningInfo) {
+        const joinItem = data.joiningInfo[s];
+        if (joinItem.id == id) {
+          console.log("found join item");
+          console.log(joinItem);
+          setStatus(prev => {prev.joinDetails = joinItem.title; return prev});
           break;
         }
       }
@@ -34,7 +38,14 @@ const Status = ({ needPopup }) => {
   return (
     <>
       {status &&
-        <Typography variant="h6">{status.title}</Typography>
+        <div style={{ marginLeft: "1rem", marginTop: "1rem" }}>
+          <Typography variant="h6">{status.title}</Typography>
+          <Typography variant="body1">Description: {status.description}</Typography>
+          <Typography variant="body1">Format: {status.format}</Typography>
+          <Typography variant="body1">Incentive: {status.incentiveAmount}</Typography>
+          <Typography variant="body1">Rounds: {status.rounds}</Typography>
+          <Typography variant="body1">Model Type: {status.modelType}</Typography>
+        </div>
       }
     </>
   )
