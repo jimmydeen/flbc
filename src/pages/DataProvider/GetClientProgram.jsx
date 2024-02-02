a2ba1c424a8da2636a89bc34691ec498a069ce3d
Edward Lim <ed.lim199@gmail.com>
2024-02-02 14:28:58 +1100

    start client (DataProvider) makes right fetch calls

--- src/pages/DataProvider/GetClientProgram.jsx
+++ src/pages/DataProvider/GetClientProgram.jsx
@@ -1,10 +1,46 @@
-import React from "react";
-import { Button } from "@mui/material";
+import React, {useState} from "react";
+import { Button, Typography, TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const GetClientProgram = () => {
+
+  const navigate = useNavigate();
+  const [clientAddress, setClientAddress] = useState();
+  const [clientPk, setClientPk] = useState();
+
+  const handleClick = (number) => {
+
+    const makeRequest = () => {
+      fetch("http://127.0.0.1:5000/start_clien", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+          body: JSON.stringify({
+            _client_address: clientAddress,
+            _client_pk: clientPk,
+          })
+      })
+      .then(res => res.json())
+      .then(data => {
+        navigate("/progress/1294801");
+      })
+      .catch(err => console.error(err))
+    }
+
+    return makeRequest;
+  }
+
   return (
     <>
-      <Button>Start Client</Button>
+      <div style={{margin: "1rem"}}>
+        <TextField id="outlined-basic" required label="Address" variant="outlined" onChange={(e) => setClientAddress(e.target.value)}/>
+        <TextField id="outlined-basic" required label="Private Key" variant="outlined" onChange={(e) => setClientPk(e.target.value)}/>
+        <Typography> Please choose one of the following to run.</Typography>
+        <Button variant="outlined" onClick={handleClick(1)}>Start Client1</Button>
+        <Button variant="outlined" onClick={handleClick(2)}>Start Client2</Button>
+        <Button variant="outlined" onClick={handleClick(3)}>Start Client3</Button>
+      </div>
     </>
   )
 }
