a8a53aef72e5ef34dfd96d27f35990256461bed5
Edward Lim <ed.lim199@gmail.com>
2024-02-02 14:31:51 +1100

    tiny mistype

--- src/pages/DataProvider/GetClientProgram.jsx
+++ src/pages/DataProvider/GetClientProgram.jsx
@@ -11,7 +11,7 @@ const GetClientProgram = () => {
   const handleClick = (number) => {
 
     const makeRequest = () => {
-      fetch("http://127.0.0.1:5000/start_clien", {
+      fetch("http://127.0.0.1:5000/start_client", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
