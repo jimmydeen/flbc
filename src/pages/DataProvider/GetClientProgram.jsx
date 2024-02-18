7fcead32fe83632b39144a78eb7df4dff85da3c7
Edward Lim <ed.lim199@gmail.com>
2024-02-19 10:30:56 +1100

    download client along with start client buttons on /getClientProgram

--- src/pages/DataProvider/GetClientProgram.jsx
+++ src/pages/DataProvider/GetClientProgram.jsx
@@ -8,10 +8,38 @@ const GetClientProgram = () => {
   const [clientAddress, setClientAddress] = useState();
   const [clientPk, setClientPk] = useState();
 
-  const handleClick = (number) => {
+  const downloadClient = () => {
+    fetch(`http://127.0.0.1:8000/download_client`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+        body: JSON.stringify({
+          _client_address: clientAddress,
+          _client_pk: clientPk,
+        })
+    })
+    .then(res => res.blob())
+    .then(blob => {
+      const blobUrl = window.URL.createObjectURL(blob);
 
+      const anchor = document.createElement('a');
+      anchor.href = blobUrl;
+      anchor.download = "client.py";
+      anchor.style.display = "none";
+      document.body.appendChild(anchor);
+      anchor.click();
+
+      anchor.remove();
+      window.URL.revokeObjectURL(blobUrl);
+      navigate("/progress/1294801")
+    })
+    .catch(err => console.error(err))
+  }
+
+  const startClient = (number) => {
     const makeRequest = () => {
-      fetch(`http://127.0.0.1:8000/download_client`, {
+      fetch(`http://127.0.0.1:8000/start_client${number}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -21,24 +49,13 @@ const GetClientProgram = () => {
             _client_pk: clientPk,
           })
       })
-      .then(res => res.blob())
-      .then(blob => {
-        const blobUrl = window.URL.createObjectURL(blob);
-
-        const anchor = document.createElement('a');
-        anchor.href = blobUrl;
-        anchor.download = "client.py";
-        anchor.style.display = "none";
-        document.body.appendChild(anchor);
-        anchor.click();
-
-        anchor.remove();
-        window.URL.revokeObjectURL(blobUrl);
-        navigate("/progress/1294801")
+      .then(res => {
+        if (res.ok) {
+          navigate("/progress/1294801")
+        }
       })
       .catch(err => console.error(err))
     }
-
     return makeRequest;
   }
 
@@ -48,9 +65,10 @@ const GetClientProgram = () => {
         <TextField id="outlined-basic" required label="Address" variant="outlined" onChange={(e) => setClientAddress(e.target.value)}/>
         <TextField id="outlined-basic" required label="Private Key" variant="outlined" onChange={(e) => setClientPk(e.target.value)}/>
         <Typography> Please choose one of the following to run.</Typography>
-        <Button variant="outlined" onClick={handleClick(1)}>Start Client1</Button>
-        <Button variant="outlined" onClick={handleClick(2)}>Start Client2</Button>
-        <Button variant="outlined" onClick={handleClick(3)}>Start Client3</Button>
+        <Button variant="outlined" onClick={startClient(1)}>Start Client1</Button>
+        <Button variant="outlined" onClick={startClient(2)}>Start Client2</Button>
+        <Button variant="outlined" onClick={startClient(3)}>Start Client3</Button>
+        <Button variant="outlined" onClick={downloadClient}>Download Client</Button>
       </div>
     </>
   )
